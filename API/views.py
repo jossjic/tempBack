b25cbaae1cfb73de2f2d07cf07f39519fd3a6f1e
mariorcbqa <mario.negrete@cbqasolutions.com>
2024-10-03 16:33:09 -0600

    setup swagger and JWT

--- API/views.py
+++ API/views.py
@@ -1,12 +1,14 @@
 import os
-
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from django.conf import settings
 from SDPM_base.models import Item
 from .serializers import  ItemSerializer
 import pysd
+
+
 
 @api_view(['GET'])
 def getData(request):
@@ -22,6 +24,27 @@ def addItem(request):
     return Response(serializer.data)
 
 
+@swagger_auto_schema(
+    method='post',
+    operation_description="Endpoint que ejecuta un modelo y devuelve una matriz de resultados según el parámetro 'Birth Rate'.",
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'Birth Rate': openapi.Schema(type=openapi.TYPE_NUMBER, description='Tasa de nacimiento', example=1.5)
+        },
+        required=['Birth Rate']
+    ),
+    responses={
+        200: openapi.Response('OK', openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'Population': openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Items(type=openapi.TYPE_NUMBER), description='Datos de población'),
+                'index': openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Items(type=openapi.TYPE_STRING), description='Fechas de la simulación')
+            }
+        )),
+        400: 'Bad Request: Birth Rate parameter is missing or invalid'
+    }
+)
 @api_view(['POST'])
 def getBunnies(request):
     model_file_path = os.path.join(settings.BASE_DIR, 'API', 'assets', 'test.mdl')
@@ -40,3 +63,8 @@ def getBunnies(request):
     print(result.index)
     print(result['Population'])
     return Response(result)
+
+
+@api_view(['POST'])
+def helloWorld(request):
+    return Response({"message": "Hello World!"})
